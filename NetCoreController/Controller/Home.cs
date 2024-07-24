d50d8968755ef2e6dab1976272c1b7190d1282f3
Om Soni <itsomsoni@gmail.com>
2024-07-24 12:46:03 +0530

    First

---
+++ NetCoreController/Controller/Home.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreController.Controller
+{
+    [Controller] //sufixing Home Class As Controller
+    public class Home
+    {
+        //all public method are called action in controller.
+        [HttpGet]
+        [Route("Home")]
+        [Route("/")]
+        public string Index()
+        {
+            return "WelCome To .NetCore Application.";
+        }
+        [Route("About")]
+        public string About()
+        {
+            return "WelCome To About Page.";
+        }
+        [Route("Contact-Us")]
+        public string Contact()
+        {
+            return "WelCome To Contact Page.";
+        }
+        [Route("/Product/{id:int:min(1000):max(9999)}")]
+        public string Product()
+        {
+            return "WelCome To Product Page.";
+        }
+    }
+}
