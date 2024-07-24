a4706dd74d4c84a709980967eab68b6ebaca2220
Om Soni <itsomsoni@gmail.com>
2024-07-24 11:54:34 +0530

    Add project files.

---
+++ NetCoreController/Program.cs
@@ -0,0 +1,19 @@
+using NetCoreController.Controller;
+
+var builder = WebApplication.CreateBuilder(args);
+
+///builder.Services.AddTransient<HomeController>();
+builder.Services.AddControllers(); // will detect all controller.
+
+var app = builder.Build();
+
+//app.UseRouting();
+
+app.MapControllers(); // will call UseRouting & UseEndpoints auto.
+
+//app.UseEndpoints(endpoints =>
+//{
+//    _ = endpoints.MapControllers();
+//});
+
+app.Run();
